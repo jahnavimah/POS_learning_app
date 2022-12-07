e080cf762e5688e4dd07bdac8d807a3f918217c9
jahnavimah <mahjahnavi@gmail.com>
2022-12-07 20:11:19 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -9,7 +9,7 @@ layout = html.Div([
     html.H1("", style={"color": "Green",'font-size':'44px','font-weight': 'bold',\
         'font-family': "Arial",'text-align': 'center'}),
     html.Div([
-        html.Div(id='gator', style={'width': 680,'height': 500,'background-image': 'url("/assets/homepage3.jpg")',\
+        html.Div(id='gator', style={'width': 680,'height': 500,'background-image': 'url("/assets/homepage4.jpg")',\
             'border': '5px ridge purple','display': 'block','margin': '0 auto'}),
             ]),
 ])
